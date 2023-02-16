779abb669f23c14ba3437b90817a55d340a57711
Alycat <Arc183@student.aru.ac.uk>
2023-02-16 07:51:39 +0000

    Added Crust Types

--- Program.cs
+++ Program.cs
@@ -15,11 +15,20 @@ string pizzabase;
         Console.WriteLine("Are you interested in a dessert pizza today? If so enter y in not enter n.");
         ynresponse = Console.ReadLine();
             //if y then proceed to next phase if n then have message, thank you for chosing Aly's Pizza
-        Console.WriteLine ("To Begin Your Order Please Choose One of the Three Pizza Bases! Type Your Choice.");
+        Console.WriteLine ("To Begin Your Order Please Choose One of the Three Pizza Bases! Please Type Your Choice.");
         Console.WriteLine ("Vanilla");
         Console.WriteLine ("Chocolate");
         Console.WriteLine ("Strawberry");
         pizzabase = Console.ReadLine ();
+            //add a part that if spelled wrong prompt again with Order line one
+        Console.WriteLine ($"{pizzabase}? Awesome!");
+        Console.WriteLine ("Now Please Choose Your Crust! Type it Below.");
+        Console.WriteLine ("Thick");
+        Console.WriteLine ("Thin");
+        Console.WriteLine ("Stuffed");
+        Console.WriteLine ("Gluton-Free");
+        Console.WriteLine ("Sugar-Free");
+
 
         Console.ReadKey(true);
 
