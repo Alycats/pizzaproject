17a91a771382acb43604357eb5a832ad1fedf2a1
Alycat <Arc183@student.aru.ac.uk>
2023-02-16 14:56:16 +0000

    Small Chnages/ Added Numbers

--- Program.cs
+++ Program.cs
@@ -2,22 +2,25 @@
 class Program
 {
 
-    static string username ="";
+    static string pizzatoppings1 = "";
+    static string pizzatoppings2 = "";
+    static string pizzatoppings3 = "";
+    static string username = "";
     static string ynresponse = "y";
-    static string pizzabase ="";
-    static string pizzacrust ="";
+    static string pizzabase = "";
+    static string pizzacrust = "";
     static void Main(string[] args)
     {
 
 
         Console.WriteLine("Please Enter Your First Name");
-        username = Console.ReadLine();
+            username = Console.ReadLine();
         Console.WriteLine($"Welcome to Aly's Pizza, {username}!");
-        Console.WriteLine("Are you interested in a dessert pizza today? If so enter y in not enter n.");
-        ynresponse = Console.ReadLine();
+        Console.WriteLine("Are you interested in a dessert pizza today? If so enter y if not enter n.");
+             ynresponse = Console.ReadLine();
         if (ynresponse == "y")
         {
-            Console.WriteLine ("To Begin Your Order Please Choose One of the Three Pizza Bases! Please Type Your Choice.");
+            Console.WriteLine ("To Begin Your Order Please Choose One of the Three Pizza Bases! Please Type Your Number Choice.");
             yresponse();
         }
         else
@@ -25,9 +28,6 @@ class Program
            nresponse();
            
         }
-
-            //if y then proceed to next phase if n then have message, thank you for chosing Aly's Pizza
-            // if y then continue, if n or others, then play message
         
     }    
     static void nresponse()
@@ -38,35 +38,38 @@ class Program
 
     static void yresponse()
     {
-        Console.WriteLine ("Vanilla");
-        Console.WriteLine ("Chocolate");
-        Console.WriteLine ("Strawberry");
-        pizzabase = Console.ReadLine ();
-            //add a part that if spelled wrong prompt again with Order line one
+        Console.WriteLine ("1-Vanilla");
+        Console.WriteLine ("2-Chocolate");
+        Console.WriteLine ("3-Strawberry");
+             pizzabase = Console.ReadLine ();
             //Maybe only numbers with the words?
         Console.WriteLine ($"{pizzabase}? Awesome!");
-        Console.WriteLine ("Now Please Choose Your Crust! Type it Below.");
-        Console.WriteLine ("Thick");
-        Console.WriteLine ("Thin");
-        Console.WriteLine ("Stuffed");
-        Console.WriteLine ("Gluton-Free");
-        Console.WriteLine ("Sugar-Free");
-        pizzacrust = Console.ReadLine();
+        Console.WriteLine ("Now Please Choose Your Crust! Type the Number Below.");
+        Console.WriteLine ("1-Thick");
+        Console.WriteLine ("2-Thin");
+        Console.WriteLine ("3-Stuffed");
+        Console.WriteLine ("4-Gluton-Free");
+        Console.WriteLine ("5-Sugar-Free");
+             pizzacrust = Console.ReadLine();
             //if option 1,2,..,5 then proceed if not then repeat
         Console.WriteLine ($"So Far We Have a {pizzabase} Pizza with {pizzacrust} Crust. Pizza-tastic!");
         Console.WriteLine ("Lastly, Let's Choose some Toppings! You Can Choose up to Three Toppings per Pizza!");
         Console.WriteLine ("Please Enter the Corrosponding Number One at a Time.");
-        Console.WriteLine ("1");
-        Console.WriteLine ("2");
-        Console.WriteLine ("3");
-        Console.WriteLine ("4");
-        Console.WriteLine ("5");
-        Console.WriteLine ("6");
-        Console.WriteLine ("7");
-        Console.WriteLine ("8");
-        Console.WriteLine ("9");
+        Console.WriteLine ("1-Chocolate Sprinkles");
+        Console.WriteLine ("2-Rainbow Sprinkles");
+        Console.WriteLine ("3-Marshmellows");
+        Console.WriteLine ("4-Generic Brand Candies");
+        Console.WriteLine ("5-Gummies");
+        Console.WriteLine ("6-Fruits");
+        Console.WriteLine ("7-Whipped Creams");
+        Console.WriteLine ("8-White Chocolates");
+        Console.WriteLine ("9-Hazelnut Spread");
             // I believe I need an Int for this and the values will have to have the names show only.
             // Prices may be stored in a seperate area? Voidspace?
+             pizzatoppings1 = Console.ReadLine();
+             pizzatoppings2 = Console.ReadLine();
+             pizzatoppings3 = Console.ReadLine();
+
         Console.ReadKey(true);
     }
 }
