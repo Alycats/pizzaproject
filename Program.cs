7c184fbbb0bfff0edac02e0db4463e822d80cf16
Alycat <Arc183@student.aru.ac.uk>
2023-03-23 13:09:53 +0000

    A lot! Added Array, Prices, and balances

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ class Program
     static string ynresponse = "";
     static int[] pizzabaseChoices = new int[1];
     static string[] pizzabaseNames = new string[3];
-    static double[] pizzabasePrice = new double [3];
+    static double[] pizzabasePrice = new double[3];
     static int[] pizzacrustChoices = new int[1];
     static string[] pizzacrustNames = new string[5];
     static double[] pizzacrustPrice = new double[5];
@@ -24,8 +24,8 @@ class Program
 
         pizzatoppingNames = new string[9] { "Chocolate Sprinkles", "Rainbow Sprinkles", "Marshmellows", "Generic Brand Candies", "Gummies", "Fruits", "Whipped Creams", "White Chocolates", "Hazelnut Spread" };
         pizzatoppingsPrice = new double[9] { 0.25, 0.25, 0.50, 0.75, 0.75, 1.00, 0.25, 0.75, 1.00 };
-        pizzabaseNames = new string[3] {"Vanilla", "Chocolate", "Strawberry"};
-        pizzabasePrice = new double[3] { 5.00, 5.50, 6.25};
+        pizzabaseNames = new string[3] { "Vanilla", "Chocolate", "Strawberry" };
+        pizzabasePrice = new double[3] { 5.00, 5.50, 6.25 };
 
         Console.WriteLine("Please Enter Your First Name");
         username = Console.ReadLine();
@@ -53,13 +53,21 @@ class Program
     static void yresponse()
     {
         for (int Pbase = 0; Pbase < 3; Pbase++)
-                    {
-                        Console.WriteLine($"{Pbase + 1} {pizzabaseNames[Pbase]}");
-                    }
+        {
+            Console.WriteLine($"{Pbase + 1} {pizzabaseNames[Pbase]}");
+        }
         bool valid = false;
         do
         {
-            pizzabaseChoices[0] = Convert.ToInt32 (Console.ReadLine());
+            pizzabaseChoices[0] = -1;
+            try
+            {
+                pizzabaseChoices[0] = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Not a Number!");
+            }
             //Maybe only numbers with the words?
             if (pizzabaseChoices[0] == 1 || pizzabaseChoices[0] == 2 || pizzabaseChoices[0] == 3)
             {
@@ -76,19 +84,29 @@ class Program
 
         static void PizzaBaseStart()
         {
+            pizzacrustNames = new string[5] { "Thick", "Thin", "Stuffed", "Gluton-Free", "Sugar-Free" };
+            pizzacrustPrice = new double[5] { 0.50, 0.50, 1.25, 0.25, 0.25 };
+
             Console.WriteLine($"{pizzabaseChoices[0]}? Awesome!");
             Console.WriteLine("Now Please Choose Your Crust! Type the Number Below.");
-            Console.WriteLine("1-Thick");
-            Console.WriteLine("2-Thin");
-            Console.WriteLine("3-Stuffed");
-            Console.WriteLine("4-Gluton-Free");
-            Console.WriteLine("5-Sugar-Free");
+            for (int crust = 0; crust < 5; crust++)
+            {
+                Console.WriteLine($"{crust + 1} {pizzacrustNames[crust]}");
+            }
             bool valid = false;
             do
             {
-                pizzacrust = Console.ReadLine();
+                pizzacrustChoices[0] = -1;
+                try
+                {
+                    pizzacrustChoices[0] = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Not a Number!");
+                }
                 //Maybe only numbers with the words?
-                if (pizzacrust == "1" || pizzacrust == "2" || pizzacrust == "3" || pizzacrust == "4" || pizzacrust == "5")
+                if (pizzacrustChoices[0] == 1 || pizzacrustChoices[0] == 2 || pizzacrustChoices[0] == 3 || pizzacrustChoices[0] == 4 || pizzacrustChoices[0] == 5)
                 {
                     valid = true;
                     PizzaToppingStart();
@@ -104,15 +122,33 @@ class Program
             {
                 for (int i = 0; i < 3; i++)
                 {
-                    Console.WriteLine($"So Far We Have a {pizzabaseChoices} Pizza with {pizzacrust} Crust. Pizza-tastic!");
+                    Console.WriteLine($"So Far We Have a {pizzabaseChoices[0]} Pizza with {pizzacrustChoices[0]} Crust. Pizza-tastic!");
                     Console.WriteLine("Lastly, Let's Choose some Toppings! You Can Choose up to Three Toppings per Pizza!");
                     Console.WriteLine("Please Enter the Corrosponding Number One at a Time.");
                     for (int topping = 0; topping < 9; topping++)
                     {
                         Console.WriteLine($"{topping + 1} {pizzatoppingNames[topping]}");
                     }
+                    bool valid = false;
+                    do
+                    {
+                        pizzatoppingsChoices[0] = Convert.ToInt32(Console.ReadLine());
+                        if (pizzatoppingsChoices[0] == 1 || pizzatoppingsChoices[0] == 2 || pizzatoppingsChoices[0] == 3 || pizzatoppingsChoices[0] == 4 || pizzatoppingsChoices[0] == 5 || pizzatoppingsChoices[0] == 6 || pizzatoppingsChoices[0] == 7 || pizzatoppingsChoices[0] == 8 || pizzatoppingsChoices[0] == 9)
+                        {
+                            valid = true;
+                            Console.WriteLine($"So Your Order is {pizzabaseChoices[0]}, with {pizzacrustChoices[0]} crust and topped with {pizzatoppingsChoices[0]}, {pizzatoppingsChoices[1]}, and {pizzatoppingsChoices[2]}. ");
+                            prices();
+                        }
+                        else
+                        {
+                            valid = false;
+                            Console.WriteLine("Invaid Response Please Try Again");
+                        }
+
+                    }
+                    while (valid == false);
                     // Prices may be stored in a seperate area? Voidspace?/loop  
-                    pizzatoppingsChoices[i] = Convert.ToInt32(Console.ReadLine());
+
                 }
 
             }
@@ -121,20 +157,17 @@ class Program
 
             }
         }
-        Console.WriteLine($"So Your Order is {pizzabaseChoices[0]}, with {pizzacrust} crust and topped with {pizzatoppingsChoices[0]}, {pizzatoppingsChoices[1]}, and {pizzatoppingsChoices[2]}. ");
 
-        prices();
         Console.ReadKey(true);
     }
     static void prices()
     {
-        double pizzabase = 10;
-        double pizzacrust = 2.50;
+
         double price1 = 0;
         double price2 = 0;
         double total = 0;
 
-        price1 = pizzabase + pizzacrust + pizzatoppingsPrice[pizzatoppingsChoices[0] - 1] + pizzatoppingsPrice[pizzatoppingsChoices[1] - 1] + pizzatoppingsPrice[pizzatoppingsChoices[2] - 1];
+        price1 = pizzabasePrice[pizzabaseChoices[0] - 1] + pizzacrustPrice[pizzacrustChoices[0] - 1] + pizzatoppingsPrice[pizzatoppingsChoices[0] - 1] + pizzatoppingsPrice[pizzatoppingsChoices[1] - 1] + pizzatoppingsPrice[pizzatoppingsChoices[2] - 1];
         price2 = price1 / 2.50;
         total = price1 + price2;
         Console.WriteLine($"Your Total Today, Including a 2.5% VAT, Will Be = ${total:f2}");
