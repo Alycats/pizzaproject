4c53c140433de2dcf6566b9bb1823ef43dd75c3a
Alycat <Arc183@student.aru.ac.uk>
2023-03-23 11:05:38 +0000

    Added array and for loop for prices and totals

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@
 class Program
 {
 
-    static string pizzatoppings1 = "";
-    static string pizzatoppings2 = "";
-    static string pizzatoppings3 = "";
+    static int[] pizzatoppingsChoices = new int[3];
+    static string[] pizzatoppingNames = new string[9];
+    static double[] pizzatoppingsPrice = new double[9];
     static string username = "";
     static string ynresponse = "";
     static string pizzabase = "";
@@ -18,7 +18,8 @@ class Program
     static void PizzaProjectMain()
     {
 
-
+        pizzatoppingNames = new string[9] { "Chocolate Sprinkles", "Rainbow Sprinkles", "Marshmellows", "Generic Brand Candies", "Gummies", "Fruits", "Whipped Creams", "White Chocolates", "Hazelnut Spread" };
+        pizzatoppingsPrice = new double[9] { 0.25, 0.25, 0.50, 0.75, 0.75, 1.00, 0.25, 0.75, 1.00 };
         Console.WriteLine("Please Enter Your First Name");
         username = Console.ReadLine();
         Console.WriteLine($"Welcome to Aly's Pizza, {username}!");
@@ -75,50 +76,44 @@ class Program
             Console.WriteLine("4-Gluton-Free");
             Console.WriteLine("5-Sugar-Free");
             bool valid = false;
-             do
-        {
-            pizzacrust = Console.ReadLine();
-            //Maybe only numbers with the words?
-            if (pizzacrust == "1" || pizzacrust == "2" || pizzacrust == "3" || pizzacrust == "4" || pizzacrust == "5")
+            do
             {
-                valid = true;
-                PizzaToppingStart();
+                pizzacrust = Console.ReadLine();
+                //Maybe only numbers with the words?
+                if (pizzacrust == "1" || pizzacrust == "2" || pizzacrust == "3" || pizzacrust == "4" || pizzacrust == "5")
+                {
+                    valid = true;
+                    PizzaToppingStart();
+                }
+                else
+                {
+                    valid = false;
+                    Console.WriteLine("Invaid Response Please Try Again");
+                }
             }
-            else
-            {
-                valid = false;
-                Console.WriteLine("Invaid Response Please Try Again");
-            }
-        }
             while (valid == false);
-         static void PizzaToppingStart()
-        {
-            Console.WriteLine($"So Far We Have a {pizzabase} Pizza with {pizzacrust} Crust. Pizza-tastic!");
-            Console.WriteLine("Lastly, Let's Choose some Toppings! You Can Choose up to Three Toppings per Pizza!");
-            Console.WriteLine("Please Enter the Corrosponding Number One at a Time.");
-            Console.WriteLine("1-Chocolate Sprinkles");
-            Console.WriteLine("2-Rainbow Sprinkles");
-            Console.WriteLine("3-Marshmellows");
-            Console.WriteLine("4-Generic Brand Candies");
-            Console.WriteLine("5-Gummies");
-            Console.WriteLine("6-Fruits");
-            Console.WriteLine("7-Whipped Creams");
-            Console.WriteLine("8-White Chocolates");
-            Console.WriteLine("9-Hazelnut Spread");
-            // Prices may be stored in a seperate area? Voidspace?/loop
-            
-            for (int i=0; i<=3; i++ )
-        {
-            pizzatoppings1 = Console.ReadLine();
-            pizzatoppings2 = Console.ReadLine();
-            pizzatoppings3 = Console.ReadLine();
-        }
-           if (pizzatoppings1 == "1")
-           {
+            static void PizzaToppingStart()
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine($"So Far We Have a {pizzabase} Pizza with {pizzacrust} Crust. Pizza-tastic!");
+                    Console.WriteLine("Lastly, Let's Choose some Toppings! You Can Choose up to Three Toppings per Pizza!");
+                    Console.WriteLine("Please Enter the Corrosponding Number One at a Time.");
+                    for (int topping = 0; topping < 9; topping++)
+                    {
+                        Console.WriteLine($"{topping + 1} {pizzatoppingNames[topping]}");
+                    }
+                    // Prices may be stored in a seperate area? Voidspace?/loop  
+                        pizzatoppingsChoices[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                
+            }
 
-           }
+            {
+
+            }
         }
-             Console.WriteLine($"So Your Order is {pizzabase}, with {pizzacrust} crust and topped with {pizzatoppings1}, {pizzatoppings2}, and {pizzatoppings3}. ");
+        Console.WriteLine($"So Your Order is {pizzabase}, with {pizzacrust} crust and topped with {pizzatoppingsChoices[0]}, {pizzatoppingsChoices[1]}, and {pizzatoppingsChoices[2]}. ");
 
         prices();
         Console.ReadKey(true);
@@ -127,23 +122,19 @@ class Program
     {
         double pizzabase = 10;
         double pizzacrust = 2.50;
-        double pizzatoppings1 = 1;
-        double pizzatoppings2 = 1;
-        double pizzatopping3 = 1;
         double price1 = 0;
         double price2 = 0;
         double total = 0;
 
-        price1 = pizzabase + pizzacrust + pizzatoppings1 + pizzatoppings2 + pizzatopping3;
+        price1 = pizzabase + pizzacrust + pizzatoppingsPrice[pizzatoppingsChoices[0]-1] + pizzatoppingsPrice[pizzatoppingsChoices[1]-1] + pizzatoppingsPrice[pizzatoppingsChoices[2]-1];
         price2 = price1 / 2.50;
         total = price1 + price2;
-        Console.WriteLine($"Your Total Today, Including a 2.5% VAT, Will Be = ${total}");
+        Console.WriteLine($"Your Total Today, Including a 2.5% VAT, Will Be = ${total:f2}");
 
 
         //Can add a yes or no question that loops back to beginning if yes or ends prgram if not- Loop
         Console.ReadKey(true);
 
-    }
     }
 }
 //CAN'T CHANGE THE WORDS TO NUMBERS, MESSES WITH MY PRETTY TEXT FIX AGAIN AND AGAIN AND AGAIN AHHHHHHHHHHHHHHHH
